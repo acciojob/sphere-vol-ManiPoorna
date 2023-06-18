95da11838d0b893a378cdf628b2dcdbf98e629df
Mani Poorna <118202451+ManiPoorna@users.noreply.github.com>
2023-06-18 12:00:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function volume_sphere() {
     //Write your code here
+    const radius = document.getElementById('radius');
+    const rad = Number(radius.value);
+    const volume = (4/3) * 22/7 * rad*rad*rad;
+    const vol = document.getElementById("volume");
+    vol.value = volume;
   
 } 
 
